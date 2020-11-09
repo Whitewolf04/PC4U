2f6e5460a6c02609e6eafdcaa2ef7aa39d0d07a9
Full Name <bm.alkhudairy@hotmail.com>
2020-11-09 15:54:28 -0500

    Lots of changes to menu, added new menu-related pages, added changes from Minh Tuấn pull request.

--- DIY_BuildPage/menu.js
+++ DIY_BuildPage/menu.js
@@ -1,4 +1,19 @@
 function displayAccountOptions()
 {
-    
+    document.getElementById("accountOptions").style.display = "block";
+}
+
+function hideAccountOptions()
+{
+    document.getElementById("accountOptions").style.display = "none";
+}
+
+function glowShoppingCart()
+{
+    document.getElementById("shoppingCart").src = "shopping_cart_hover.png";
+}
+
+function dullShoppingCart()
+{
+    document.getElementById("shoppingCart").src = "shopping_cart_nohover.png";
 }
