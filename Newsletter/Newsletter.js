c287292a6e728f541e87865c0ceafd2713e1eecc
UlyanaI <71348979+UlyanaI@users.noreply.github.com>
2020-11-09 22:04:33 -0500

    Add files via upload

---
+++ Newsletter/Newsletter.js
@@ -0,0 +1,17 @@
+function overImage(img){
+    img.style.width = "125%"; 
+    
+}
+
+function outImage(img){
+    img.style.width = "75%"; 
+}
+
+function openSubs() {
+    document.getElementById("pop-subscribe").style.display="block";
+}
+
+function closeSubs() {
+    document.getElementById("pop-subscribe").style.display="none";
+}
+
