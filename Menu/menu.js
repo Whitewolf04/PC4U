3e7f51c7a182d94016fed8219242077d90e2f34f
Bilal Samee <bm.alkhudairy@hotmail.com>
2020-11-26 03:33:12 -0500

    Fixed menu

--- Menu/menu.js
+++ Menu/menu.js
@@ -1,19 +1,9 @@
 document.addEventListener("DOMContentLoaded", function()
 {
     var shoppingCart = document.getElementById("shoppingCart");
-    var accountButton = document.getElementById("accountButton");
-    var loginButton = document.getElementById("loginButton");
-    var registerButton = document.getElementById("registerButton");
 
     shoppingCart.addEventListener("mouseover", glowShoppingCart);
     shoppingCart.addEventListener("mouseout", dullShoppingCart);
-
-    accountButton.addEventListener("mouseover", displayAccountOptions);
-    loginButton.addEventListener("mouseover", displayAccountOptions);
-    registerButton.addEventListener("mouseover", displayAccountOptions);
-    accountButton.addEventListener("mouseout", hideAccountOptions);
-    loginButton.addEventListener("mouseout", hideAccountOptions);
-    registerButton.addEventListener("mouseout", hideAccountOptions);
 });
 
 function displayAccountOptions()
--- Menu/menu.php
+++ Menu/menu.php
@@ -5,27 +5,27 @@
         <script type="text/javascript" src="../Menu/menu.js"></script>
     </head>
     <body>
-        <form id="menu">
-            <table id="buttonGroup"><tr>
-                <td><img src="../logo.png" width="50" height="50" id="logo"/></td>
-                <td><a class="menuButton" href="../DIY_BuildPage/DIY_Mainpage.html">Main Page</a></td>
-                <td><a class="menuButton" href="../Newsletter/Newsletter.html">Newsletter</a></td>
-                <td><a class="menuButton" href="../DIY_BuildPage/DIY_GUIDE_PAGE.html">Tutorial</a></td>
-            </tr></table>
+        <div id="menu">
+            <ul id="buttonGroup">
+                <li><img src="../logo.png" width="50" height="50" id="logo"/></li>
+                <li><a class="menuButton" href="../DIY_BuildPage/DIY_Mainpage.html">Main Page</a></li>
+                <li><a class="menuButton" href="../Newsletter/Newsletter.html">Newsletter</a></li>
+                <li><a class="menuButton" href="../DIY_BuildPage/DIY_GUIDE_PAGE.html">Tutorial</a></li>
+            </ul>
 
-            <table id="buttonGroup"><tr>
-                <div id="accountMenu">
-                    <td><a id="accountButton" class="menuButton" href="#">Account</a></td>
+            <ul id="accountGroup">
+                <li id="accountMenu">
+                    <a id="accountButton" class="menuButton" href="#">Account</a>
                     <div id="accountOptions">
                         <a id="loginButton" class="menuButton" href="../Menu/login.php">Login</a>
                         <a id="registerButton" class="menuButton" href="../Menu/register.php">Register</a>
                     </div>
-                </div>
-                <td><a id="shoppingCart" href="#">
-                    <img id="shoppingCartBot" src="../Menu/Images/shopping_cart_hover.png" width="30px" height="30px" />
+                </li>
+                <li><a id="shoppingCart" href="#">
+                    <img src="../Menu/Images/shopping_cart_hover.png" width="30px" height="30px" />
                     <img id="shoppingCartTop" src="../Menu/Images/shopping_cart_nohover.png" width="30px" height="30px" />
-                </a></td>
-            </tr></table>
-        </form>
+                </a></li>
+            </ul>
+        </div>
     </body>
 </html>
