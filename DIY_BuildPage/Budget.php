1df3fd1d68556ed6b26131d6e06b16e651f0b7d9
Whitewolf04 <tominhtuan18@gmail.com>
2020-12-04 18:58:55 -0500

    Fixed Budget Build page

--- DIY_BuildPage/Budget.php
+++ DIY_BuildPage/Budget.php
@@ -30,11 +30,12 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>PC4U</title>
-	<link rel="stylesheet" type="text/css" href="Buildpage.css" />
+	<link rel="stylesheet" type="text/css" href="Budget.css" />
 	<meta name="viewport" content="width=device-width, initial-scale=1">
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<<<<<<< Updated upstream
 	<style>
 		table#builds {
 			margin: auto;
@@ -156,6 +157,8 @@
 			margin: auto;
 		}
 	</style>
+=======
+>>>>>>> Stashed changes
 </head>
 
 		<div id="odd">
@@ -364,13 +367,13 @@
 				<td>308.0</td>
 			</tr>
 	<!--Might do a form to ask what are the needs of the customer!-->
-	<table id="builds" border="0" cellspacing="20px">
+	<table class="builds" border="0" cellspacing="20px">
 		<tr class="build">
 			<td class="picture">
-				<img src="Images/DIYPC_MA08.jpg" width="300px" height="300px" />
-				<p id="price">$400</p>
+				<img src="Images/DIYPC_MA08.png" width="300px" height="300px" />
 			</td>
-			<td colspan="2">
+			<td colspan="2" class="specs">
+				<p class="price">Price: $400</p><br>
 				<p>Specification:</p>
 				<p>CPU: AMD Ryzen 3 3200G</p>
 				<p>Motherboard: MSI A320M-A PRO MAX Micro ATX AM4 Motherboard</p>
@@ -378,37 +381,41 @@
 				<p>Storage: Team GX1 240 GB 2.5" Solid State Drive</p>
 				<p>Case: DIYPC MA08 Micro ATX Mini Tower</p>
 				<p>Power Supply: Cooler Master Elite V3 400W 80+ Certified</p>
+				<br>
+				<button class="cart">Add to cart</button>
 			</td>
 		</tr>
 		<tr class="fps">
-			<td class="fps">
-				<img src="Images/LeagueOfLegends.jpg" width="180" height="240" />
-				<p>High Settings</p><br>
+			<td class="fpsleft">
+				<img src="Images/LeagueOfLegends.jpg" width="180" height="240" /><br><br>
+				<p>High Settings</p>
 				<div class="progress" style="width: 40%;">
 					<div class="progress-bar" role="progressbar" aria-valuenow="97" aria-valuemin="0" aria-valuemax="200" style="width: 48.5%;">97 fps</div>
 				</div>
 			</td>
 			<td class="fps">
-				<img src="Images/csgo.png" width="230" height="240" />
-				<p>High Settings</p><br>
-				<div class="progress" style="width: 40%;">
+				<img src="Images/csgo.png" width="230" height="240" /><br><br>
+				<p>High Settings</p>
+				<div class="progress" style="width: 50%;">
 					<div class="progress-bar" role="progressbar" aria-valuenow="74.3" aria-valuemin="0" aria-valuemax="200" style="width: 37.2%;">74.3 fps</div>
 				</div>
 			</td>
-			<td class="fps">
-				<img src="Images/fortnite.jpg" width="180" height="240" />
-				<p>Low Settings</p><br>
+			<td class="fpsright">
+				<img src="Images/fortnite.jpg" width="180" height="240" /><br><br>
+				<p>Low Settings</p>
 				<div class="progress" style="width: 40%;">
 					<div class="progress-bar" role="progressbar" aria-valuenow="61.7" aria-valuemin="0" aria-valuemax="200" style="width: 31%;">61.7 fps</div>
 				</div>
 			</td>
 		</tr>
+
+	<table class="builds" border="0" cellspacing="20px">
 		<tr class="build">
 			<td class="picture">
-				<img src="Images/CMN200.jpg" width="300px" height="300px" />
-				<p>$750</p>
+				<img src="Images/CMN200.png" width="300px" height="300px" />
 			</td>
-			<td colspan="2">
+			<td colspan="2" class="specs">
+				<p class="price">Price: $750</p><br>
 				<p>Specification:</p>
 				<p>CPU: AMD Ryzen 3 3100</p>
 				<p>Motherboard: AsRock A520M-HDV Micro ATX AM4 Motherboard</p>
@@ -417,37 +424,42 @@
 				<p>Storage: Team GX1 240 GB 2.5" Solid State Drive</p>
 				<p>Case: Cooler Master N200 Micro ATX Mini Tower</p>
 				<p>Power Supply: Cooler Master Elite V3 400W 80+ Certified</p>
+				<br>
+				<button class="cart">Add to cart</button>
 			</td>
 		</tr>
 		<tr class="fps">
-			<td class="fps">
-				<img src="Images/LeagueOfLegends.jpg" width="180" height="240" />
-				<p>Ultra Settings</p><br>
+			<td class="fpsleft">
+				<img src="Images/LeagueOfLegends.jpg" width="180" height="240" /><br><br>
+				<p>Ultra Settings</p>
 				<div class="progress" style="width: 40%;">
 					<div class="progress-bar" role="progressbar" aria-valuenow="200" aria-valuemin="0" aria-valuemax="200" style="width: 100%;">384.8 fps</div>
 				</div>
 			</td>
 			<td class="fps">
-				<img src="Images/shadowOfTombRaider.jpg" width="180" height="240" />
-				<p>High Settings</p><br>
-				<div class="progress" style="width: 40%;">
+				<img src="Images/shadowOfTombRaider.jpg" width="180" height="240" /><br><br>
+				<p>High Settings</p>
+				<div class="progress" style="width: 60%;">
 					<div class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="200" style="width: 49.5%;">99 fps</div>
 				</div>
 			</td>
-			<td class="fps">
-				<img src="Images/ACOdyssey.png" width="180" height="240" />
-				<p>Medium Settings</p><br>
+			<td class="fpsright">
+				<img src="Images/ACOdyssey.png" width="180" height="240" /><br><br>
+				<p>Medium Settings</p>
 				<div class="progress" style="width: 40%;">
 					<div class="progress-bar" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="200" style="width: 45%;">90 fps</div>
 				</div>
 			</td>
 		</tr>
+	</table>
+
+	<table class="builds" border="0" cellspacing="20px">
 		<tr class="build">
 			<td class="picture">
-				<img src="Images/CMN200.jpg" width="300px" height="300px" />
-				<p>$730</p>
+				<img src="Images/CMN200.png" width="300px" height="300px" />
 			</td>
-			<td colspan="2">
+			<td colspan="2" class="specs">
+				<p class="price">Price: $730</p><br>
 				<p>Specification:</p>
 				<p>CPU: Intel Core i3-9100F 3.6 GHz Quad-Core Processor</p>
 				<p>Motherboard: MSI B365M PRO-VDH Micro ATX Motherboard</p>
@@ -456,28 +468,42 @@
 				<p>Storage: Team GX1 240 GB 2.5" Solid State Drive</p>
 				<p>Case: Cooler Master N200 Micro ATX Mini Tower</p>
 				<p>Power Supply: Thermaltake Smart Series 430W 80+ Certified</p>
+				<br>
+				<button class="cart">Add to cart</button>
 			</td>
 		</tr>
 		<tr class="fps">
-			<td class="fps">
-				<img src="Images/LeagueOfLegends.jpg" width="180" height="240" />
-				<p>Ultra Settings: 376.8 fps</p>
+			<td class="fpsleft">
+				<img src="Images/LeagueOfLegends.jpg" width="180" height="240" /><br><br>
+				<p>Ultra Settings</p>
+				<div class="progress" style="width: 40%;">
+					<div class="progress-bar" role="progressbar" aria-valuenow="200" aria-valuemin="0" aria-valuemax="200" style="width: 100%;">376.8 fps</div>
+				</div>
 			</td>
 			<td class="fps">
-				<img src="Images/shadowOfTombRaider.jpg" width="180" height="240" />
-				<p>High Settings: 94.9 fps</p>
+				<img src="Images/shadowOfTombRaider.jpg" width="180" height="240" /><br><br>
+				<p>High Settings</p>
+				<div class="progress" style="width: 60%;">
+					<div class="progress-bar" role="progressbar" aria-valuenow="94.9" aria-valuemin="0" aria-valuemax="200" style="width: 47.5%;">94.9 fps</div>
+				</div>
 			</td>
-			<td class="fps">
-				<img src="Images/ACOdyssey.png" width="180" height="240" />
-				<p>Medium Settings: 95 fps</p>
+			<td class="fpsright">
+				<img src="Images/ACOdyssey.png" width="180" height="240" /><br><br>
+				<p>Medium Settings</p>
+				<div class="progress" style="width: 40%;">
+					<div class="progress-bar" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="200" style="width: 47.5%;">95 fps</div>
+				</div>
 			</td>
 		</tr>
+	</table>
+
+	<table class="builds" border="0" cellspacing="20px">
 		<tr class="build">
 			<td class="picture">
-				<img src="Images/CMN200.jpg" width="300px" height="300px" />
-				<p>$950</p>
+				<img src="Images/CMN200.png" width="300px" height="300px" />
 			</td>
-			<td colspan="2">
+			<td colspan="2" class="specs">
+				<p class="price">Price: $950</p><br>
 				<p>Specification:</p>
 				<p>CPU: AMD Ryzen 5 3600 3.6 GHz 6-Core Processor</p>
 				<p>Motherboard: AsRock B550-HDV Micro ATX AM4 Motherboard</p>
@@ -486,20 +512,31 @@
 				<p>Storage: Kingston A400 240 GB 2.5" Solid State Drive</p>
 				<p>Case: Cooler Master N200 Micro ATX Mini Tower</p>
 				<p>Power Supply: Thermaltake Smart 500W 80+ Certified</p>
+				<br>
+				<button class="cart">Add to cart</button>
 			</td>
 		</tr>
 		<tr class="fps">
-			<td class="fps">
-				<img src="Images/csgo.png" width="220" height="240" />
-				<p>Ultra Settings: 262.7 fps</p>
+			<td class="fpsleft">
+				<img src="Images/csgo.png" width="220" height="240" /><br><br>
+				<p>Ultra Settings</p>
+				<div class="progress" style="width: 40%;">
+					<div class="progress-bar" role="progressbar" aria-valuenow="200" aria-valuemin="0" aria-valuemax="200" style="width: 100%;">262.7 fps</div>
+				</div>
 			</td>
 			<td class="fps">
-				<img src="Images/rdr2.jpg" width="180" height="240" />
-				<p>Medium Settings: 111.7 fps</p>
+				<img src="Images/rdr2.jpg" width="180" height="240" /><br><br>
+				<p>Medium Settings</p>
+				<div class="progress" style="width: 60%;">
+					<div class="progress-bar" role="progressbar" aria-valuenow="111.7" aria-valuemin="0" aria-valuemax="200" style="width: 55.9%;">111.7 fps</div>
+				</div>
 			</td>
-			<td class="fps">
-				<img src="Images/ACOdyssey.png" width="180" height="240" />
-				<p>High Settings: 82.3 fps</p>
+			<td class="fpsright">
+				<img src="Images/ACOdyssey.png" width="180" height="240" /><br><br>
+				<p>High Settings</p>
+				<div class="progress" style="width: 40%;">
+					<div class="progress-bar" role="progressbar" aria-valuenow="82.3" aria-valuemin="0" aria-valuemax="200" style="width: 41.2%;">82.3 fps</div>
+				</div>
 			</td>
 		</tr>
 	</table>
