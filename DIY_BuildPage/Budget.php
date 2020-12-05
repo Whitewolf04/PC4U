fb6352f5e04b438546dd190826433a41d3c38f79
Whitewolf04 <tominhtuan18@gmail.com>
2020-12-05 14:53:12 -0500

    Changing Budget.php to dynamic

--- DIY_BuildPage/Budget.php
+++ DIY_BuildPage/Budget.php
@@ -8,31 +8,80 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+	<style>
+		td {
+			width: 33.3333333%;
+		}
+	</style>
 </head>
 
 <body>
-	<?php require_once "../Menu/nav.php" ?>
+	<?php 
+		require_once "../Menu/nav.php";
+
+		function buildSearch($lines, $buildname){
+			$line = "";
+			for($i = 0; $i < count($lines); $i++){
+				$line = explode("\t", $lines[$i]);
+				if(strcmp($line[0], $buildname) == 0){
+					return $line;
+				}
+			}
+			$line = "";
+			return $line;
+		}
+
+		function readSpecs($buildname){
+			$prebuilds = fopen("../Database/prebuilds.txt", "r");
+			$contents = fread($prebuilds, filesize("../Database/prebuilds.txt"));
+			$lines = explode("\n", $contents);
+			$line = buildSearch($lines, $buildname);
+			fclose($prebuilds);
+
+			if(!empty($line)){
+				$specs = explode(", ", $line[1]);
+				$price = $line[2];
+				$cpu = $specs[0];
+				$cpuCooler = $specs[1];
+				$mobo = $specs[2];
+				$ram = $specs[3];
+				$gpu = $specs[4];
+				$ssd = $specs[5];
+				$hdd = $specs[6];
+				$case = $specs[7];
+				$psu = $specs[8];
+				
+				$printHDD = "<p>Storage HDD: ".$hdd."</p>";
+				$printCpuCooler = "<p>CPU Cooler: ".$cpuCooler."</p>";
+				$printSpec = "<p class=\"price\">Price: $".$price."</p><br>\n
+						<p>Specification:</p>\n
+						<p>CPU: ".$cpu."</p>\n".(empty($cpuCooler)?"":$printCpuCooler)."\n
+						<p>Motherboard: ".$mobo."</p>\n
+						<p>RAM: ".$ram."</p>\n
+						<p>GPU: ".$gpu."</p>\n
+						<p>Storage SSD: ".$ssd."</p>\n".(empty($hdd)?"":$printHDD)."\n
+						<p>Case: ".$case."</p>\n
+						<p>Power Supply: ".$psu."</p><br>\n";
+
+				echo $printSpec;
+			}
+		}
+	?>
 
 	<h1 id="banner">Budget Builds</h1>
 
 	<!--Might do a form to ask what are the needs of the customer!-->
-	<table class="builds" border="0" cellspacing="20px">
+	<form action="cart.php" method="POST" id="budget5"><table class="builds" border="0" cellspacing="20px">
 		<tr class="build">
 			<td class="picture">
 				<img src="Images/DIYPC_MA08.png" width="300px" height="300px" />
 			</td>
 			<td colspan="2" class="specs">
-				<p class="price">Price: $400</p><br>
-				<p>Specification:</p>
-				<p>CPU: AMD Ryzen 3 3200G</p>
-				<p>Motherboard: MSI A320M-A PRO MAX Micro ATX AM4 Motherboard</p>
-				<p>RAM: G.Skill Aegis 8 GB DDR4-3000 CL 16 Memory</p>
-				<p>Storage: Team GX1 240 GB 2.5" Solid State Drive</p>
-				<p>Case: DIYPC MA08 Micro ATX Mini Tower</p>
-				<p>Power Supply: Cooler Master Elite V3 400W 80+ Certified</p>
-				<br>
-				<button class="cart">Add to cart</button>
+				<?php
+					readSpecs("budget5");
+				?>
+				<button type="submit" form="budget5" class="cart" value="budget5" >Add to cart</button>
 			</td>
 		</tr>
 		<tr class="fps">
@@ -46,7 +95,7 @@
 			<td class="fps">
 				<img src="Images/csgo.png" width="230" height="240" /><br><br>
 				<p>High Settings</p>
-				<div class="progress" style="width: 50%;">
+				<div class="progress" style="width: 30%;">
 					<div class="progress-bar" role="progressbar" aria-valuenow="74.3" aria-valuemin="0" aria-valuemax="200" style="width: 37.2%;">74.3 fps</div>
 				</div>
 			</td>
@@ -58,6 +107,7 @@
 				</div>
 			</td>
 		</tr>
+	</table></form>
 
 	<table class="builds" border="0" cellspacing="20px">
 		<tr class="build">
@@ -65,17 +115,10 @@
 				<img src="Images/CMN200.png" width="300px" height="300px" />
 			</td>
 			<td colspan="2" class="specs">
-				<p class="price">Price: $750</p><br>
-				<p>Specification:</p>
-				<p>CPU: AMD Ryzen 3 3100</p>
-				<p>Motherboard: AsRock A520M-HDV Micro ATX AM4 Motherboard</p>
-				<p>RAM: G.Skill Aegis 8 GB DDR4-3200 CL 16 Memory</p>
-				<p>GPU: Asus GeForce GTX 1650 Super 4 GB Strix Gaming Advanced</p>
-				<p>Storage: Team GX1 240 GB 2.5" Solid State Drive</p>
-				<p>Case: Cooler Master N200 Micro ATX Mini Tower</p>
-				<p>Power Supply: Cooler Master Elite V3 400W 80+ Certified</p>
-				<br>
-				<button class="cart">Add to cart</button>
+				<?php
+					readSpecs("budget3");
+				?>
+				<button class="cart" value="750">Add to cart</button>
 			</td>
 		</tr>
 		<tr class="fps">
@@ -89,7 +132,7 @@
 			<td class="fps">
 				<img src="Images/shadowOfTombRaider.jpg" width="180" height="240" /><br><br>
 				<p>High Settings</p>
-				<div class="progress" style="width: 60%;">
+				<div class="progress" style="width: 30%;">
 					<div class="progress-bar" role="progressbar" aria-valuenow="99" aria-valuemin="0" aria-valuemax="200" style="width: 49.5%;">99 fps</div>
 				</div>
 			</td>
@@ -109,17 +152,10 @@
 				<img src="Images/CMN200.png" width="300px" height="300px" />
 			</td>
 			<td colspan="2" class="specs">
-				<p class="price">Price: $730</p><br>
-				<p>Specification:</p>
-				<p>CPU: Intel Core i3-9100F 3.6 GHz Quad-Core Processor</p>
-				<p>Motherboard: MSI B365M PRO-VDH Micro ATX Motherboard</p>
-				<p>RAM: G.Skill Aegis 16 GB (2 x 8 GB) DDR4-2666 CL 19 Memory</p>
-				<p>GPU: Asus GeForce GTX 1650 Super 4 GB Strix Gaming Advanced</p>
-				<p>Storage: Team GX1 240 GB 2.5" Solid State Drive</p>
-				<p>Case: Cooler Master N200 Micro ATX Mini Tower</p>
-				<p>Power Supply: Thermaltake Smart Series 430W 80+ Certified</p>
-				<br>
-				<button class="cart">Add to cart</button>
+				<?php
+					readSpecs("budget2");
+				?>
+				<button class="cart" value="730">Add to cart</button>
 			</td>
 		</tr>
 		<tr class="fps">
@@ -133,7 +169,7 @@
 			<td class="fps">
 				<img src="Images/shadowOfTombRaider.jpg" width="180" height="240" /><br><br>
 				<p>High Settings</p>
-				<div class="progress" style="width: 60%;">
+				<div class="progress" style="width: 30%;">
 					<div class="progress-bar" role="progressbar" aria-valuenow="94.9" aria-valuemin="0" aria-valuemax="200" style="width: 47.5%;">94.9 fps</div>
 				</div>
 			</td>
@@ -153,17 +189,10 @@
 				<img src="Images/CMN200.png" width="300px" height="300px" />
 			</td>
 			<td colspan="2" class="specs">
-				<p class="price">Price: $950</p><br>
-				<p>Specification:</p>
-				<p>CPU: AMD Ryzen 5 3600 3.6 GHz 6-Core Processor</p>
-				<p>Motherboard: AsRock B550-HDV Micro ATX AM4 Motherboard</p>
-				<p>RAM: Team T-Force Vulcan Z 16 GB (2 x 8 GB) DDR4-3200 CL 16 Memory</p>
-				<p>GPU: MSI GeForce GTX 1600 Super 6 GB Ventus XS OC</p>
-				<p>Storage: Kingston A400 240 GB 2.5" Solid State Drive</p>
-				<p>Case: Cooler Master N200 Micro ATX Mini Tower</p>
-				<p>Power Supply: Thermaltake Smart 500W 80+ Certified</p>
-				<br>
-				<button class="cart">Add to cart</button>
+				<?php
+					readSpecs("budget1");
+				?>
+				<button class="cart" value="950">Add to cart</button>
 			</td>
 		</tr>
 		<tr class="fps">
@@ -177,7 +206,7 @@
 			<td class="fps">
 				<img src="Images/rdr2.jpg" width="180" height="240" /><br><br>
 				<p>Medium Settings</p>
-				<div class="progress" style="width: 60%;">
+				<div class="progress" style="width: 30%;">
 					<div class="progress-bar" role="progressbar" aria-valuenow="111.7" aria-valuemin="0" aria-valuemax="200" style="width: 55.9%;">111.7 fps</div>
 				</div>
 			</td>
