c5ff7572e3d0f04e0f0008e9d4f6497160764b03
arnoldT25 <72999190+arnoldT25@users.noreply.github.com>
2020-12-04 23:32:37 -0500

    Update Highend.php

--- DIY_BuildPage/Highend.php
+++ DIY_BuildPage/Highend.php
@@ -1,243 +1,117 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8"/>
-        <title>PC4U</title>
-		<link rel="stylesheet" type="text/css" href="Buildpage.css"/>
-	</head>
 
-    <body>
-		<?php require_once "../Menu/nav.php" ?>
+<head>
+    <meta charset="UTF-8" />
+    <title>PC4U</title>
+    <link rel="stylesheet" type="text/css" href="NewBuildpage.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
 
-    	<h1 id="banner">High End Builds</h1>
+<body>
+    <?php require_once "../Menu/nav.php" ?>
 
-        <!--Might do a form to ask what are the needs of the customer!-->
-        <h2>PCs from $2000+ of the month</h2>
-		<p><a href="BudgetSort.html">Sort from Low to High</a></p>
-		
-		<div id="even">
-        <table border ="2" rules="none" id="spec">
-        	<caption>(AMD)</caption>
-        	<tr>
-        		<td>Component:</td>
-        		<td>Selection</td>
-        		<td>Price</td>
-        	</tr>
-        	<tr>
-        		<td>CPU</td>
-        		<td>AMD Ryzen 7 5800X 3.8 GHz 8-Core Processor</td>
-        		<td>$629.99</td>
-        	</tr>
-            <tr>
-                <td>CPU Cooler</td>
-                <td>NZXT Kraken X63 98.17 CFM Liquid CPU Cooler</td>
-                <td>$195.99</td>
-            </tr>
-        	<tr>
-        		<td>Motherboard</td>
-        		<td>MSI MAG X570 TOMAHAWK WIFI ATX AM4 Motherboard</td>
-        		<td>$319.00</td>
-        	</tr>
-        	<tr>
-        		<td>Memory</td>
-        		<td>G.Skill Trident Z RGB 16 GB (2 x 8 GB) DDR4-3600 CL18 Memory</td>
-        		<td>$114.99</td>
-        	</tr>
-        	<tr>
-        		<td>Storage</td>
-        		<td>Crucial P1 500 GB M.2-2280 NVME Solid State Drive</td>
-        		<td>$71.99</td>
-        	</tr>
-        	<tr>
-        		<td>Video Card</td>
-        		<td>EVGA GeForce RTX 3070 8 GB XC3 BLACK GAMING Video Card</td>
-        		<td>$719.99</td>
-        	</tr>
-        	<tr>
-        		<td>Case</td>
-        		<td>Corsair iCUE 220T RGB Airflow ATX Mid Tower Case</td>
-        		<td>$129.99</td>
-        	</tr>
-        	<tr>
-        		<td>Power Supply</td>
-        		<td>Cooler Master MWE Gold 750 W 80+ Gold Certified Fully Modular ATX Power Supply</td>
-        		<td>$174.99</td>
-        	</tr>
-        	<tr>
-        		<td><a href="https://ca.pcpartpicker.com/user/whitewolf04/saved/?fbclid=IwAR0ukIcZw-NrplTQiLkslEac_IwVQMajjcc8l8a-tpjAqUp92NfRE5q4JvM#view=gLbgbv">Buy Now</a></td>
-        		<td>Total: </td>
-        		<td>$2356.93</td>
-        	</tr>
-        </table>
-        <p></p>
-        <table border="2">
-        	<caption>Performance in Games at 1080p</caption>
-        	<tr>
-        		<td>Games</td>
-        		<td>Settings</td>
-        		<td>Minimum FPS</td>
-        	</tr>
-        	<tr>
-        		<td>Assasin's Creed Odyssey</td>
-        		<td>Ultra Quality</td>
-        		<td>95.2</td>
-        	</tr>
-        	<tr>
-        		<td>Forza Horizon 4</td>
-        		<td>Ultra Quality</td>
-        		<td>181.7</td>
-        	</tr>
-        	<tr>
-        		<td>Shadow of the Tomb Raider</td>
-        		<td>Ultra Quality</td>
-        		<td>148.0</td>
-        	</tr>
-        	<tr>
-        		<td>Player Unknown's Battleground</td>
-        		<td>Ultra Quality</td>
-        		<td>148.0</td>
-        	</tr>
-        	<tr>
-        		<td>Battlefield V</td>
-        		<td>Ultra Quality</td>
-        		<td>184.5</td>
-        	</tr>
-        	<tr>
-        		<td>Apex Legends</td>
-        		<td>Ultra Quality</td>
-        		<td>168.0</td>
-        	</tr>
-        	<tr>
-        		<td>Overwatch</td>
-        		<td>Ultra Quality</td>
-        		<td>253.0</td>
-        	</tr>
-        	<tr>
-        		<td>Counter Strike: Global Offensive</td>
-        		<td>Ultra Quality</td>
-        		<td>459.0</td>
-        	</tr>
-            <tr>
-                <td>Rainbow Six Siege</td>
-                <td>Ultra Quality</td>
-                <td>324.0</td>
-			</tr>
-        	
-		</table>
-		</div>
+    <h1 id="banner">High-End Builds</h1>
 
-		<div id="odd">
-        <p></p>
-        <table border="2" rules="none" id="spec">
-        	<caption>(AMD)</caption>
-        	<tr>
-        		<td>Component:</td>
-        		<td>Selection</td>
-        		<td>Price</td>
-        	</tr>
-        	<tr>
-        		<td>CPU</td>
-        		<td>AMD Ryzen 5 5600X 3.7 GHz 6-Core Processor</td>
-        		<td>$449.99</td>
-        	</tr>
-            <tr>
-                <td>CPU Cooler</td>
-                <td>Noctua NH-U12S 55 CFM CPU Cooler</td>
-                <td>$79.75</td>
-            </tr>
-        	<tr>
-        		<td>Motherboard</td>
-        		<td>Asus TUF GAMING X570-PLUS (WI-FI) ATX AM4 Motherboard</td>
-        		<td>$249.00</td>
-        	</tr>
-        	<tr>
-        		<td>Memory</td>
-        		<td>G.Skill Trident Z RGB 16 GB (2 x 8 GB) DDR4-3600 CL18 Memory</td>
-        		<td>$114.99</td>
-        	</tr>
-        	<tr>
-        		<td>Storage</td>
-        		<td>Crucial P1 500 GB M.2-2280 NVME Solid State Drive</td>
-        		<td>$71.99</td>
-        	</tr>
-        	<tr>
-        		<td>Video Card</td>
-        		<td>EVGA GeForce RTX 3080 10 GB FTW3 ULTRA GAMING Video Card</td>
-        		<td>$1109.00</td>
-        	</tr>
-        	<tr>
-        		<td>Case</td>
-        		<td>Phanteks Eclipse P400A Digital ATX Mid Tower Case</td>
-        		<td>$129.99</td>
-        	</tr>
-        	<tr>
-        		<td>Power Supply</td>
-        		<td>Corsair RM (2019) 850 W 80+ Gold Certified Fully Modular ATX Power Supply</td>
-        		<td>$149.99</td>
-        	</tr>
-        	<tr>
-        		<td><a href="https://ca.pcpartpicker.com/user/whitewolf04/saved/?fbclid=IwAR0ukIcZw-NrplTQiLkslEac_IwVQMajjcc8l8a-tpjAqUp92NfRE5q4JvM#view=QBFk99">Buy Now</a></td>
-        		<td>Total: </td>
-        		<td>$2354.70</td>
-        	</tr>
-        </table>
-        <table border="2">
-        	<caption>Performance in Games at 1080p</caption>
-        	<tr>
-        		<td>Games</td>
-        		<td>Settings</td>
-        		<td>Minimum FPS</td>
-        	</tr>
-        	<tr>
-        		<td>Assasin's Creed Odyssey</td>
-        		<td>Ultra Quality</td>
-        		<td>103.3</td>
-        	</tr>
-        	<tr>
-        		<td>Forza Horizon 4</td>
-        		<td>Ultra Quality</td>
-        		<td>196.2</td>
-        	</tr>
-        	<tr>
-        		<td>Shadow of the Tomb Raider</td>
-        		<td>Ultra Quality</td>
-        		<td>159.7</td>
-        	</tr>
-        	<tr>
-        		<td>Player Unknown's Battleground</td>
-        		<td>Ultra Quality</td>
-        		<td>159.7</td>
-        	</tr>
-        	<tr>
-        		<td>Battlefield V</td>
-        		<td>Ultra Quality</td>
-        		<td>199.4</td>
-        	</tr>
-        	<tr>
-        		<td>Apex Legends</td>
-        		<td>Ultra Quality</td>
-        		<td>160.0</td>
-        	</tr>
-        	<tr>
-        		<td>Overwatch</td>
-        		<td>Ultra Quality</td>
-        		<td>241.0</td>
-        	</tr>
-        	<tr>
-        		<td>Counter Strike: Global Offensive</td>
-        		<td>Ultra Quality</td>
-        		<td>437.0</td>
-        	</tr>
-            <tr>
-                <td>Rainbow Six Siege</td>
-                <td>Ultra Quality</td>
-                <td>309.0</td>
-            </tr>
-        	
-        </table>
-		<p></p>
-		</div>
-    
-        <footer>Note: All prices are in Canadian Dollar, and peripherals are not included</footer>
-    </body>
+    <!--Might do a form to ask what are the needs of the customer!-->
+    <table class="builds" border="0" cellspacing="20px">
+        <tr class="build">
+            <td class="picture">
+                <img src="Images/corsairicue.jpg" width="300px" height="300px" />
+            </td>
+            <td colspan="2" class="specs">
+                <p class="price">Price: $2856</p><br>
+                <p>Specification:</p>
+                <p>CPU: AMD Ryzen 7 5800X 3.8 Ghz (Up to 4.7 GHz) 8-Core Processor</p>
+                <p>CPU Cooler:  NZXT Kraken X63 98.17 CFM Liquid CPU Cooler</p>
+                <p>Motherboard: MSI Mag X570 Tomahawk Wifi ATX AM4 Motherboard</p>
+                <p>RAM: G.Skill Trident Z RGB 16 GB (2 x 8 GB) DDR4-3600 CL18 Memory</p>
+                <p>GPU: Asus GeForce RTX 3080 10 GB STRIX GAMING OC Video Card</p>
+                <p>Storage SSD: Crucial P1 500 GB M.2-2280 NVME Solid State Drive</p>
+                <p>Storage HDD: Seagate Barracuda Compute 2 TB 3.5" 7200RPM Internal Hard Drive</p>
+                <p>Case: Corsair iCUE 220T RGB Airflow ATX Mid Tower Case</p>
+                <p>Power Supply: Cooler Master MWE Gold 750 W 80+ Gold Certified Fully Modular ATX Power Supply</p>
+                <br>
+                <button class="cart">Add to cart</button>
+            </td>
+        </tr>
+        <tr class="fps">
+            <td class="fpsleft">
+                <img src="Images/shadowOfTombRaider.jpg" width="180" height="240" /><br><br>
+                <p>High Settings</p>
+                <div class="progress" style="width: 40%;">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="193.5" aria-valuemin="0" aria-valuemax="200" style="width: 75.6%;">193.5 fps</div>
+                </div>
+            </td>
+            <td class="fps">
+                <img src="Images/rdr2.jpg" width="180" height="240" /><br><br>
+                <p>High Settings</p>
+                <div class="progress" style="width: 50%;">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="107.0" aria-valuemin="0" aria-valuemax="200" style="width: 42.7%;">107.0 fps</div>
+                </div>
+            </td>
+            <td class="fpsright">
+                <img src="Images/ACValhalla.jpg" width="180" height="240" /><br><br>
+                <p>High Settings</p>
+                <div class="progress" style="width: 40%;">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="136.0" aria-valuemin="0" aria-valuemax="200" style="width: 41.6%;">136.0 fps</div>
+                </div>
+            </td>
+        </tr>
+    </table>
+
+    <table class="builds" border="0" cellspacing="20px">
+        <tr class="build">
+            <td class="picture">
+                <img src="Images/lianli.jpg" width="300px" height="300px" />
+            </td>
+            <td colspan="2" class="specs">
+                <p class="price">Price: $3120</p><br>
+                <p>Specification:</p>
+                <p>CPU: Intel Core i9-10900K 3.7 (Up to 5.3 GHz) 10-Core Processor</p>
+                <p>CPU Cooler: Corsair H100i RGB PLATINUM 75 CFM Liquid CPU Cooler</p>
+                <p>Motherboard: Asus ROG STRIX Z490-E GAMING ATX LGA1200 Motherboard</p>
+                <p>RAM: G.Skill Trident Z RGB 16 GB (2 x 8 GB) DDR4-3600 CL18 Memory</p>
+                <p>GPU: Asus GeForce RTX 3080 10 GB STRIX GAMING OC Video Card</p>
+                <p>Storage SSD: Samsung 970 Evo 500 GB M.2-2280 NVME Solid State Drive</p>
+                <p>Storage HDD: Seagate Barracuda Compute 2 TB 3.5" 7200RPM Internal Hard Drive</p>
+                <p>Case: Lian Li Lancool II Mesh ATX Mid Tower Case</p>
+                <p>Power Supply: Corsair RM (2019) 750 W 80+ Gold Certified Fully Modular ATX Power Supply</p>
+                <br>
+                <button class="cart">Add to cart</button>
+            </td>
+        </tr>
+        <tr class="fps">
+            <td class="fpsleft">
+                <img src="Images/shadowOfTombRaider.jpg" width="180" height="240" /><br><br>
+                <p>High Settings</p>
+                <div class="progress" style="width: 40%;">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="201.0" aria-valuemin="0" aria-valuemax="200" style="width: 84.6%;">201.0 fps</div>
+                </div>
+            </td>
+            <td class="fps">
+                <img src="Images/rdr2.jpg" width="180" height="240" /><br><br>
+                <p>High Settings</p>
+                <div class="progress" style="width: 50%;">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="110.0" aria-valuemin="0" aria-valuemax="200" style="width: 43%;">110.0 fps</div>
+                </div>
+            </td>
+            <td class="fpsright">
+                <img src="Images/ACValhalla.jpg" width="180" height="240" /><br><br>
+                <p>High Settings</p>
+                <div class="progress" style="width: 40%;">
+                    <div class="progress-bar" role="progressbar" aria-valuenow="130.0" aria-valuemin="0" aria-valuemax="200" style="width: 47.8%;">130.0 fps</div>
+                </div>
+            </td>
+        </tr>
+    </table>
+
+
+    <p></p>
+    <footer>Note: All prices are in Canadian Dollar, and peripherals are not included</footer>
+</body>
+
 </html>
