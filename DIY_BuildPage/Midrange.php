161b67cb66cecf738dbe7f6fa669e04477e88a23
Whitewolf04 <tominhtuan18@gmail.com>
2020-12-04 20:50:37 -0500

    Update Midrange.php

--- DIY_BuildPage/Midrange.php
+++ DIY_BuildPage/Midrange.php
@@ -1,21 +1,23 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8"/>
-        <title>PC4U</title>
-		<link rel="stylesheet" type="text/css" href="NewBuildpage.css"/>
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-    </head>
-    <body>
-		<?php require_once "../Menu/nav.php" ?>
 
-    	<h1 id="banner">Mid-Range Builds</h1>
+<head>
+	<meta charset="UTF-8" />
+	<title>PC4U</title>
+	<link rel="stylesheet" type="text/css" href="NewBuildpage.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
 
-		<!--Might do a form to ask what are the needs of the customer!-->
-		<table class="builds" border="0" cellspacing="20px">
+<body>
+	<?php require_once "../Menu/nav.php" ?>
+
+	<h1 id="banner">Mid-Range Builds</h1>
+
+	<!--Might do a form to ask what are the needs of the customer!-->
+	<table class="builds" border="0" cellspacing="20px">
 		<tr class="build">
 			<td class="picture">
 				<img src="Images/CMMB311L.png" width="300px" height="300px" />
@@ -58,6 +60,7 @@
 				</div>
 			</td>
 		</tr>
+	</table>
 
 	<table class="builds" border="0" cellspacing="20px">
 		<tr class="build">
@@ -65,60 +68,16 @@
 				<img src="Images/CMMB311L.png" width="300px" height="300px" />
 			</td>
 			<td colspan="2" class="specs">
-				<p class="price">Price: $1130</p><br>
-				<p>Specification:</p>
-				<p>CPU: Intel Core i5-10400F 2.9 GHz (Up to 4.3 GHz) 6-Core Processor</p>
-				<p>Motherboard: AsRock B460M Pro4 Micro ATX Motherboard</p>
-				<p>RAM: G.Skill Aegis 16 GB (2 x 8 GB) DDR4-2666 CL 19 Memory</p>
-				<p>GPU: PowerColor Radeon RX 5600XT 6 GB Red Devil Video Card</p>
-				<p>Storage SSD: Team GX2 512 GB 2.5" Solid State Drive</p>
-				<p>Storage HDD: Seagate Barracuda Compute 2 TB 3.5" 7200RPM Internal Hard Drive</p>
-				<p>Case: Cooler Master MasterBox MB311L ARGB MicroATX Mid Tower</p>
-				<p>Power Supply: SeaSonic S12III 500W 80+ Bronze Certified</p>
-				<br>
-				<button class="cart">Add to cart</button>
-			</td>
-		</tr>
-		<tr class="fps">
-			<td class="fpsleft">
-				<img src="Images/shadowOfTombRaider.jpg" width="180" height="240" /><br><br>
-				<p>High Settings</p>
-				<div class="progress" style="width: 40%;">
-					<div class="progress-bar" role="progressbar" aria-valuenow="152.2" aria-valuemin="0" aria-valuemax="200" style="width: 76.1%;">152.2 fps</div>
-				</div>
-			</td>
-			<td class="fps">
-				<img src="Images/rdr2.jpg" width="180" height="240" /><br><br>
-				<p>High Settings</p>
-				<div class="progress" style="width: 50%;">
-					<div class="progress-bar" role="progressbar" aria-valuenow="85.9" aria-valuemin="0" aria-valuemax="200" style="width: 43%;">85.9 fps</div>
-				</div>
-			</td>
-			<td class="fpsright">
-				<img src="Images/ACValhalla.jpg" width="180" height="240" /><br><br>
-				<p>High Settings</p>
-				<div class="progress" style="width: 40%;">
-					<div class="progress-bar" role="progressbar" aria-valuenow="83.8" aria-valuemin="0" aria-valuemax="200" style="width: 42%;">83.8 fps</div>
-				</div>
-			</td>
-		</tr>
-	</table>
-
-	<table class="builds" border="0" cellspacing="20px">
-		<tr class="build">
-			<td class="picture">
-				<img src="Images/corsair275r.png" width="300px" height="300px" />
-			</td>
-			<td colspan="2" class="specs">
-				<p class="price">Price: $1440</p><br>
+				<p class="price">Price: $1319</p><br>
 				<p>Specification:</p>
 				<p>CPU: Intel Core i5-10400F 2.9 GHz (Up to 4.3 GHz) 6-Core Processor</p>
 				<p>CPU Cooler: Noctua NH-D15</p>
 				<p>Motherboard: AsRock B460M Pro4 Micro ATX Motherboard</p>
-				<p>RAM: Corsair Vengeance LPX 16 GB (2 x 8 GB) DDR4-2666 CL16 Memory</p>
+				<p>RAM: G.Skill Aegis 16 GB (2 x 8 GB) DDR4-2666 CL19 Memory</p>
 				<p>GPU: AsRock Radeon RX 5700XT 8 GB Challenger D OC Video Card</p>
-				<p>Storage: Western Digital Blue 500 GB M.2-2280 Solid State Drive</p>
-				<p>Case: Corsair 275R Airflow ATX Mid Tower</p>
+				<p>Storage SSD: Team GX2 512 GB 2.5" Solid State Drive</p>
+				<p>Storage HDD: Seagate Barracuda Compute 2 TB 3.5" 7200RPM Internal Hard Drive</p>
+				<p>Case: Cooler Master MasterBox MB311L ARGB Micro ATX Mid Tower</p>
 				<p>Power Supply: Cooler Master Elite V3 600W 80+ Certified</p>
 				<br>
 				<button class="cart">Add to cart</button>
@@ -129,7 +88,7 @@
 				<img src="Images/shadowOfTombRaider.jpg" width="180" height="240" /><br><br>
 				<p>High Settings</p>
 				<div class="progress" style="width: 40%;">
-					<div class="progress-bar" role="progressbar" aria-valuenow="152.2" aria-valuemin="0" aria-valuemax="200" style="width: 76.1%;">152.2 fps</div>
+					<div class="progress-bar" role="progressbar" aria-valuenow="169.1" aria-valuemin="0" aria-valuemax="200" style="width: 84.6%;">169.1 fps</div>
 				</div>
 			</td>
 			<td class="fps">
@@ -152,48 +111,95 @@
 	<table class="builds" border="0" cellspacing="20px">
 		<tr class="build">
 			<td class="picture">
-				<img src="Images/CMN200.png" width="300px" height="300px" />
+				<img src="Images/corsair275r.png" width="300px" height="300px" />
 			</td>
 			<td colspan="2" class="specs">
-				<p class="price">Price: $950</p><br>
+				<p class="price">Price: $1685.47</p><br>
 				<p>Specification:</p>
-				<p>CPU: AMD Ryzen 5 3600 3.6 GHz 6-Core Processor</p>
-				<p>Motherboard: AsRock B550-HDV Micro ATX AM4 Motherboard</p>
-				<p>RAM: Team T-Force Vulcan Z 16 GB (2 x 8 GB) DDR4-3200 CL 16 Memory</p>
-				<p>GPU: MSI GeForce GTX 1600 Super 6 GB Ventus XS OC</p>
-				<p>Storage: Kingston A400 240 GB 2.5" Solid State Drive</p>
-				<p>Case: Cooler Master N200 Micro ATX Mini Tower</p>
-				<p>Power Supply: Thermaltake Smart 500W 80+ Certified</p>
+				<p>CPU: AMD Ryzen 7 3700X 3.6 GHz (Up to 4.4 GHz) 8-Core Processor</p>
+				<p>CPU Cooler: Cooler Master Hyper 212 RGB Black Edition</p>
+				<p>Motherboard: MSI B450 Tomahawk MAX ATX AM4 Motherboard</p>
+				<p>RAM: Corsair Vengeance LPX 16 GB (2 x 8 GB) DDR4-3200 CL16 Memory</p>
+				<p>GPU: EVGA GeForce RTX 3060 Ti 8 GB XC Gaming</p>
+				<p>Storage: Western Digital Blue 500 GB M.2-2280 Solid State Drive</p>
+				<p>Case: Corsair 275R Airflow ATX Mid Tower</p>
+				<p>Power Supply: SeaSonic S12III 650W 80+ Bronze Certified</p>
 				<br>
 				<button class="cart">Add to cart</button>
 			</td>
 		</tr>
 		<tr class="fps">
 			<td class="fpsleft">
-				<img src="Images/csgo.png" width="220" height="240" /><br><br>
+				<img src="Images/shadowOfTombRaider.jpg" width="180" height="240" /><br><br>
 				<p>Ultra Settings</p>
 				<div class="progress" style="width: 40%;">
-					<div class="progress-bar" role="progressbar" aria-valuenow="200" aria-valuemin="0" aria-valuemax="200" style="width: 100%;">262.7 fps</div>
+					<div class="progress-bar" role="progressbar" aria-valuenow="124.5" aria-valuemin="0" aria-valuemax="200" style="width: 62.25%;">124.5 fps</div>
 				</div>
 			</td>
 			<td class="fps">
 				<img src="Images/rdr2.jpg" width="180" height="240" /><br><br>
-				<p>Medium Settings</p>
-				<div class="progress" style="width: 60%;">
-					<div class="progress-bar" role="progressbar" aria-valuenow="111.7" aria-valuemin="0" aria-valuemax="200" style="width: 55.9%;">111.7 fps</div>
+				<p>High Settings</p>
+				<div class="progress" style="width: 50%;">
+					<div class="progress-bar" role="progressbar" aria-valuenow="117.2" aria-valuemin="0" aria-valuemax="200" style="width: 58.6%;">117.2 fps</div>
 				</div>
 			</td>
 			<td class="fpsright">
-				<img src="Images/ACOdyssey.png" width="180" height="240" /><br><br>
+				<img src="Images/ACValhalla.jpg" width="180" height="240" /><br><br>
 				<p>High Settings</p>
 				<div class="progress" style="width: 40%;">
-					<div class="progress-bar" role="progressbar" aria-valuenow="82.3" aria-valuemin="0" aria-valuemax="200" style="width: 41.2%;">82.3 fps</div>
+					<div class="progress-bar" role="progressbar" aria-valuenow="114.6" aria-valuemin="0" aria-valuemax="200" style="width: 57.3%;">114.6 fps</div>
 				</div>
 			</td>
 		</tr>
 	</table>
 
-        <p></p>
-        <footer>Note: All prices are in Canadian Dollar, and peripherals are not included</footer>
-    </body>
+	<table class="builds" border="0" cellspacing="20px">
+		<tr class="build">
+			<td class="picture">
+				<img src="Images/corsair275r.png" width="300px" height="300px" />
+			</td>
+			<td colspan="2" class="specs">
+				<p class="price">Price: $1800</p><br>
+				<p>Specification:</p>
+				<p>CPU: AMD Ryzen 7 3700X 3.6 GHz (Up to 4.4 GHz) 8-Core Processor</p>
+				<p>CPU Cooler: Cooler Master Hyper 212 RGB Black Edition</p>
+				<p>Motherboard: MSI B450 Tomahawk MAX ATX AM4 Motherboard</p>
+				<p>RAM: Corsair Vengeance LPX 16 GB (2 x 8 GB) DDR4-3200 CL16 Memory</p>
+				<p>GPU: Nvidia GeForce RTX 3070 8 GB Founders Edition</p>
+				<p>Storage: Western Digital Blue 500 GB M.2-2280 Solid State Drive</p>
+				<p>Case: Corsair 275R Airflow ATX Mid Tower</p>
+				<p>Power Supply: SeaSonic S12III 650W 80+ Bronze Certified</p>
+				<br>
+				<button class="cart">Add to cart</button>
+			</td>
+		</tr>
+		<tr class="fps">
+			<td class="fpsleft">
+				<img src="Images/shadowOfTombRaider.jpg" width="180" height="240" /><br><br>
+				<p>Ultra Settings</p>
+				<div class="progress" style="width: 40%;">
+					<div class="progress-bar" role="progressbar" aria-valuenow="148" aria-valuemin="0" aria-valuemax="200" style="width: 74%;">148 fps</div>
+				</div>
+			</td>
+			<td class="fps">
+				<img src="Images/rdr2.jpg" width="180" height="240" /><br><br>
+				<p>High Settings</p>
+				<div class="progress" style="width: 50%;">
+					<div class="progress-bar" role="progressbar" aria-valuenow="131.5" aria-valuemin="0" aria-valuemax="200" style="width: 65.8%;">131.5 fps</div>
+				</div>
+			</td>
+			<td class="fpsright">
+				<img src="Images/ACValhalla.jpg" width="180" height="240" /><br><br>
+				<p>High Settings</p>
+				<div class="progress" style="width: 40%;">
+					<div class="progress-bar" role="progressbar" aria-valuenow="129.5" aria-valuemin="0" aria-valuemax="200" style="width: 64.8%;">129.5 fps</div>
+				</div>
+			</td>
+		</tr>
+	</table>
+
+	<p></p>
+	<footer>Note: All prices are in Canadian Dollar, and peripherals are not included</footer>
+</body>
+
 </html>
