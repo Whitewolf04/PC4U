0f47cc9703c64b322e07be298e7724f94b676883
kvmn2000 <72999480+kvmn2000@users.noreply.github.com>
2020-12-05 01:24:21 -0500

    cart can now accept pcwizard parts and get them from products.txt

--- Account/cart.php
+++ Account/cart.php
@@ -17,9 +17,9 @@
                     $cart = array();
                     $prebuild = "";
                     //checks if a prebuild was added to the cart
-                    if($_SESSION['prebuild']!=null){
-                        if(file_exists("prebuilds.txt")){
-                            $stream = fopen("prebuilds.txt", "r");
+                    if(isset($_SESSION['prebuilt'])){
+                        if(file_exists("../Database/prebuilds.txt")){
+                            $stream = fopen("../Database/prebuilds.txt", "r");
                             while(($line=fgets($stream))!==false){
                                 if($line===null){
                                     continue;
@@ -31,6 +31,34 @@
                             }
                         }
                     }
+                    if(isset($_SESSION['pcWizard'])){
+                        // $pcParts = $_SESSION['pcWizard'];
+                        // for($x = 0; $x < count($pcParts); $x++){
+                        //     echo $pcParts[$x]."<br>";
+                        // }
+                        if(file_exists("../Database/products.txt")){
+                            $stream = fopen("../Database/products.txt", "r");
+                            $pcParts = $_SESSION['pcWizard'];
+                            for($x = 0; $x < count($pcParts); $x++){
+                                //echo $pcParts[$x]."<br>";
+                                $title = substr($pcParts[$x], 0, strpos($pcParts[$x], "\t"));
+                                $productCode = trim(substr($pcParts[$x], strpos($pcParts[$x], "\t")+1, strlen($pcParts[$x])-strpos($pcParts[$x], "\t")));
+                                //echo $title." ".$productCode."<br>";
+                                while(($line=fgets($stream))!==false){
+                                    if(strpos($line, $title) !== false){
+                                        if(strpos($line, $productCode)!== false){
+                                            $line = trim(substr($line, strpos($line, $productCode) + strlen($productCode), strlen($line)-strpos($line, $productCode)+ strlen($productCode)));
+                                            $name = trim(substr($line, 0, strpos($line, "\t")));
+                                            $line = trim(substr($line, strpos($line, $name) + strlen($name), strlen($line)-strpos($line, $name) + strlen($name)));
+                                            $price = trim(substr($line, 0, strpos($line, "\t")));
+                                            $cart[$name] = $price;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    print_r($cart);
                     if(isset($_POST['puchaseNow'])){
                         $_SESSION['cart'] = $cart;
                     }
