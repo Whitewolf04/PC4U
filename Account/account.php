810a9922f1badcc70ac89aaf15a44969cc1539c8
kvmn2000 <72999480+kvmn2000@users.noreply.github.com>
2020-12-06 01:29:12 -0500

    added change password and account css

--- Account/account.php
+++ Account/account.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
-<?php session_start(); ?>
+<?php //session_start(); ?>
 <html>
     <head>
 		<meta charset="UTF-8" />
 		<title>PC4U</title>
-		<link rel="stylesheet" type="text/css" href="../DIY_BuildPage/Buildpage.css" />
+        <link rel="stylesheet" type="text/css" href="../DIY_BuildPage/Buildpage.css" />
+        <link rel="stylesheet" type="text/css" href="account.css" />
 	</head>
     <body>
         <?php require_once "../Menu/nav.php" ?>
         <div class="main">
             <div class="wrap">
+                <div class="sidebar">
+                    <a href="changePassword.php">Change your password</a>
+                </div>
                 <div class="content">
-                    <?php echo "<h3>". $_SESSION['username']. "'s profile</h3>";?>
+                    <?php echo "<h3>". $_SESSION['email']. "'s profile</h3>"?>
                     <div>
                     <h3>Your recommendations</h3>
-                    <?php include "recommendations.php"?>
+                    <?php //include "recommendations.php"?>
                     </div>
-                </div>
-                <div class="sidebar">
-                <a href="">Change your password</a>
                 </div>
             </div>
         </div>
---
+++ Account/changePassword.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+	<title>PC4U</title>
+    <link rel="stylesheet" type="text/css" href="../DIY_BuildPage/Buildpage.css" />
+    <link rel="stylesheet" type="text/css" href="account.css" />
+</head>
+<body>
+<?php require_once "../Menu/nav.php" ?>
+        <div class="main">
+            <div class="wrap">
+                <div class="content">
+                    <div>
+                        <h3>Change your Password</h3>
+                        <form action="" method="POST">
+                            <input type="password" name="password" placeholder="Enter your password">
+                            <br><br>
+                            <input type="password" name="confirm" placeholder="Confirm your password">
+                            <br><br>
+                            <input type="submit" name="submit" value="Submit">
+                        </form>
+                        <?php
+                        if(isset($_POST['submit'])){
+                            $password = $confirm = "";
+                            if(!(empty($_POST['password'])||empty($_POST['confirm']))){
+                                if(strcmp($password, $confirm)==0){
+                                    $password = $_POST['password'];
+                                    $confirm = $_POST['confirm'];
+                                    //checkPassword($password);
+                                }else{
+                                    echo "Password and confirm password do not match. Please Try Again.";
+                                }
+                                
+                            }else{
+                                echo "One of the fields is missing! Please try again.";
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+                <div class="sidebar">
+                <a href="account.php">Your profile</a>
+                </div>
+            </div>
+        </div>
+        <footer>
+            
+        </footer>
+</body>
+</html>
