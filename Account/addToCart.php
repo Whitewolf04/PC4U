2c3998906a3ee7ee50096411f99f0947b2a7d6bc
kvmn2000 <72999480+kvmn2000@users.noreply.github.com>
2020-12-06 20:51:05 -0500

    added addtocart notification and session cart for payment handler

--- Account/addToCart.php
+++ Account/addToCart.php
@@ -9,6 +9,7 @@
         if(isset($_POST['prebuilt'])){
             $cookievalue = $_COOKIE['cart']."Prebuilt ".$_POST['prebuilt']."~";
             setcookie("cart", $cookievalue, false, "/");
+            echo "Added to cart!";
         }else{
             $cpu = $motherboard = $gpu = $ram = $storage = $cooler = $case = $powerSupply = "";
             if(!(empty($_POST['cpu'])||empty($_POST['mobo'])||empty($_POST['gpu'])||empty($_POST['ram'])||empty($_POST['storageAmount'])||empty($_POST['coolerSize'])||empty($_POST['caseSize'])||empty($_POST['powerWatt']))){
@@ -24,6 +25,7 @@
                 $cookievalue = $_COOKIE['cart']."PCwizard ". json_encode($pcWizard)."~";
                 //echo $cookievalue;
                 setcookie("cart", $cookievalue, false, "/");
+                echo "Added to cart!";
                 //echo $_COOKIE['cart'];
             }else{
                 echo "One of the fields was missing! Try again.<br>";
