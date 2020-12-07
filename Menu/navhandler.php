053e954262001dd612cd7f3dd9b2b651de342591
Endgineer <bm.alkhudairy@hotmail.com>
2020-12-07 02:20:09 -0500

    Signout always redirects to pc4u.php

--- Menu/navhandler.php
+++ Menu/navhandler.php
@@ -1,21 +1,16 @@
 <?php
     session_start();
 
-	if(!isset($_SESSION['redirect']))
-	{
-		$_SESSION['redirect'] = "../Newsletter/pc4u.php";
-	}
-	
-	if(!isset($_GET['signout']))
+    if(!isset($_GET['signout']))
     {
-        header("Location: ".$_SESSION['redirect']);
+        header("Location: ../Newsletter/pc4u.php");
         exit;
     }
 
     if(isset($_GET['signout']))
     {
         unset($_SESSION['signedin']);
-        header("Location: ".$_SESSION['redirect']);
+        header("Location: ../Newsletter/pc4u.php");
         exit;
     }
 ?>
